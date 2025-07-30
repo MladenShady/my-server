06993b97a2fcc61279b49986c3ae02b9b15af8b1
Mladen Mitic <mladenmitic@Mac.localdomain>
2025-07-30 19:13:08 +0200

    ruta

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Početna ruta
 app.get('/', (req, res) => {
-  res.send('Krastavac !');
+  res.send('banana');
 });
 
+// Ruta /zdravo
+app.get('/zdravo', (req, res) => {
+  res.send('Zdravo, Mladen!');
+});
+
+// Ruta /kontakt
+app.get('/kontakt', (req, res) => {
+  res.send('Kontakt stranica');
+});
+
+// Pokretanje servera
 app.listen(port, () => {
   console.log(`Server radi na portu ${port}`);
 });
